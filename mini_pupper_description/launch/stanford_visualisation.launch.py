61a4528965702e9abd15a30b787997487e4544bb
javiercweddington <javiercw@stanford.edu>
2026-01-05 21:25:45 +0000

    Update mini_pupper code

--- mini_pupper_description/launch/stanford_visualisation.launch.py
+++ mini_pupper_description/launch/stanford_visualisation.launch.py
@@ -56,6 +56,12 @@ def generate_launch_description():
             declare_description_path,
             declare_use_sim_time,
             Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='odom_to_base_link',
+                arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link']
+            ),
+            Node(
                 package='mini_pupper_description',
                 executable='stanford_joint_trajectory_to_states',
                 name='stanford_joint_trajectory_to_states',
--- stanford_controller/setup.py
+++ stanford_controller/setup.py
@@ -1,5 +1,4 @@
-from setuptools import find_packages, setup
-import os
+from setuptools import setup, find_packages
 from glob import glob
 
 package_name = 'stanford_controller'
@@ -9,21 +8,15 @@ setup(
     version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='MangDang',
-    maintainer_email='fae@mangdang.net',
-    description='A ROS 2 Python package for the StanfordController node.',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'stanford_controller_node = stanford_controller.stanford_controller_node:main',
-            'twist_to_command_node = stanford_controller.twist_to_command_node:main'
-        ],
-    },
+    maintainer='champ',
+    maintainer_email='champ@todo.com',
+    description='Stanford controller',
+    license='BSD',
 )
